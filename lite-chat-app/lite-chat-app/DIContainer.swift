bc83529e39a00b87e6d3a05424dcb71fc6e2aca6
Alexander <nickolaev513@gmail.com>
2026-03-05 11:41:43 +0300

    нормик должно быть

--- lite-chat-app/lite-chat-app/DIContainer.swift
+++ lite-chat-app/lite-chat-app/DIContainer.swift
@@ -5,7 +5,13 @@ final class DIContainer {
     static let shared = DIContainer()
     private init() {}
 
+    #if DEBUG
+    let authUseCase = AuthUseCase(repository: MockAPIService.shared)
+    let chatsUseCase = ChatsUseCase(repository: MockAPIService.shared)
+    let messagesUseCase = MessagesUseCase(repository: MockAPIService.shared)
+    #else
     let authUseCase = AuthUseCase(repository: APIService.shared)
     let chatsUseCase = ChatsUseCase(repository: APIService.shared)
     let messagesUseCase = MessagesUseCase(repository: APIService.shared)
+    #endif
 }
